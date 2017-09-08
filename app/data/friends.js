31e50ab2d7485b85821a386332877ad30262f886
Alex <alexanderbullock87@gmail.com>
2017-09-08 12:54:47 -0400

    resest data.. fixed form validation bug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 var friendsArray = [
-  {
-    name: "Ahmed",
-    photo: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg",
-    scores:["1","1","1","1","1","1","1","1","1","1"]
-  }
+  // {
+  //   name: "Ahmed",
+  //   photo: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg",
+  //   scores:["1","1","1","1","1","1","1","1","1","1"]
+  // }
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
