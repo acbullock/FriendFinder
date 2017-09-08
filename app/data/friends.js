49d73791faaedd91a1adc91e5ecadbbf23ec4d03
Alex <alexanderbullock87@gmail.com>
2017-09-08 13:13:25 -0400

    changed starting data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 var friendsArray = [
-  // {
-  //   name: "Ahmed",
-  //   photo: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg",
-  //   scores:["1","1","1","1","1","1","1","1","1","1"]
-  // }
+  {
+    name: "Charlamagne tha God",
+    photo: "http://loudspeakersnetwork.com/wp-content/uploads/2014/05/Charlamagne-Tha-God400.png",
+    scores:["4","3","5","5","4","5","1","2","2","1"]
+  }
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
